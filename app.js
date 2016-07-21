ed4e2ed313d1dd81cc8f4b15cf4e31a01eb8074e
Joshua Tokle <jtokle@gmail.com>
2016-07-21 07:10:31 -0700

    uncomment dotenv and set errors to silent
    closes #6

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var Twit = require('twit');
 var path = require('path');
 var slackbot = require('node-slackbot');
-// require('dotenv').config({
-// 	path: path.resolve('./env_variables')
-// });
+require('dotenv').config({
+	path: path.resolve('./env_variables'),
+	silent: true
+});
 
 var T = new Twit({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
