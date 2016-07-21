01fe660406941e25691f6900dad9299403ee78f8
James Zetlen <jzetlen@paypal.com>
2016-07-21 16:44:43 -0500

    chore(dev): style cleanup and compliance scripting

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var Twit = require('twit');
 var path = require('path');
-var slackbot = require('node-slackbot');
+var Slackbot = require('node-slackbot');
+
 require('dotenv').config({
   path: path.resolve('./env_variables'),
-  silent: true
+  silent: process.env.NODE_ENV !== 'development'
 });
 
 var T = new Twit({
@@ -13,9 +14,9 @@ var T = new Twit({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var bot = new slackbot(process.env.SLACK_TOKEN);
+var bot = new Slackbot(process.env.SLACK_TOKEN);
 
-function postTweet(text) {
+function postTweet (text) {
   T.post('statuses/update', { status: text }, (err, data, res) => {
     if (err) {
       console.error(err);
@@ -25,21 +26,18 @@ function postTweet(text) {
   });
 }
 
-bot.use(
-  function(message, cb) {
-  if ('pin_added' == message.type) {
+bot.use(function (message, cb) {
+  if (message.type === 'pin_added') {
     console.log('received pin', JSON.stringify(message.item, null, 2));
     var text = message.item.message && message.item.message.text;
     if (text && text.length <= 140) {
       postTweet(text);
-    } else if (text && text.length >140) {
-      var trimmedText = text.substring(0,138) + "…";
+    } else if (text && text.length > 140) {
+      var trimmedText = text.substring(0, 138) + '…';
       postTweet(trimmedText);
     }
   }
 });
 
-
-
 bot.connect();
-console.log('listening...')
+console.log('listening...');
