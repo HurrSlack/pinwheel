8a0432021f4c31dc41de443744d6d0370ccfa1e9
James Zetlen <jzetlen@paypal.com>
2016-07-21 11:02:49 -0500

    style(indents): 🌌

--- app.js
+++ app.js
@@ -2,41 +2,41 @@ var Twit = require('twit');
 var path = require('path');
 var slackbot = require('node-slackbot');
 require('dotenv').config({
-	path: path.resolve('./env_variables'),
-	silent: true
+  path: path.resolve('./env_variables'),
+  silent: true
 });
 
 var T = new Twit({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token: process.env.TWITTER_ACCESS_TOKEN,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 var bot = new slackbot(process.env.SLACK_TOKEN);
 
 function postTweet(text) {
-	T.post('statuses/update', { status: text }, (err, data, res) => {
-			if (err) {
-				console.error(err);
-			} else {
-				console.log('twote "' + text + '", received: ', data);
-			}
-	});
+  T.post('statuses/update', { status: text }, (err, data, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('twote "' + text + '", received: ', data);
+    }
+  });
 }
 
 bot.use(
-	function(message, cb) {
-	if ('pin_added' == message.type) {
-		console.log('received pin', JSON.stringify(message.item, null, 2));
-		var text = message.item.message && message.item.message.text;
-		if (text && text.length <= 140) {
-			postTweet(text);
-		} else if (text && text.length >140) {
-			var trimmedText = text.substring(0,138) + "…";
-			postTweet(trimmedText);
-		}
-	}
+  function(message, cb) {
+  if ('pin_added' == message.type) {
+    console.log('received pin', JSON.stringify(message.item, null, 2));
+    var text = message.item.message && message.item.message.text;
+    if (text && text.length <= 140) {
+      postTweet(text);
+    } else if (text && text.length >140) {
+      var trimmedText = text.substring(0,138) + "…";
+      postTweet(trimmedText);
+    }
+  }
 });
 
 
