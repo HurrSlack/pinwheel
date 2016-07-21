ff0d30b201c8b801f6872cdab6c1ba8a59d06d64
Charles Vestal <charlesv@gmail.com>
2016-07-21 14:30:07 -0700

    Working text wrapping
    currently barcoded to write a png

--- app.js
+++ app.js
@@ -17,7 +17,33 @@ var T = new Twit({
 var bot = new slackbot(process.env.SLACK_TOKEN);
 
 var Canvas = require('canvas')
+var CanvasTextWrapper = require('canvas-text-wrapper').CanvasTextWrapper;
+
+// tests
+var text = "You'd take the clothes off my back and I'd let you You'd steal the food right out my mouth and I'd watch you eat it I still don’t know why, why our love is so much, ohh (Thanks for warnin' me, thanks for warnin' me) You curse my name, in spite, to put me to shame Hang my laundry in the streets, dirty or clean, give it up for fame But I still don't know why, why I love it so much (Thanks for warnin' me, thanks for warnin' me)";
  
+var trimmedText = text.substring(0,110) + "…";
+var canvas = new Canvas(880, 400);
+createSlackImage(canvas, text);
+
+var imageData =  canvas.toDataURL('image/png'); 
+
+var fs = require('fs')
+  , out = fs.createWriteStream(__dirname + '/text.png')
+  , stream = canvas.pngStream();
+
+stream.on('data', function(chunk){
+  out.write(chunk);
+});
+
+stream.on('end', function(){
+  console.log('saved png');
+});
+
+
+
+// end tests
+
 
 function postTweet(text) {
 	T.post('statuses/update', { status: text }, (err, data, res) => {
@@ -51,8 +77,25 @@ function createSlackImage(canvas, text) {
     var Image = Canvas.Image
     , ctx = canvas.getContext('2d');
 
-  	ctx.font = '24px Slack-Lato';
-	var image = ctx.fillText(text, 20, 25);
+	var options = { 
+        font: "24px Arial, sans-serif",
+        lineHeight: 1,
+        textAlign: "left",
+        verticalAlign: "top",
+        paddingX: 0,
+        paddingY: 0,
+        fitParent: false,
+        lineBreak: "auto",
+        sizeToFill: false,
+        allowNewLine: true,
+        justifyLines: false,
+        strokeText: false,
+        renderHDPI: true,
+        textDecoration: "none"
+    }
+	
+	CanvasTextWrapper(canvas, text, options);
+
 }
 
 bot.use(
