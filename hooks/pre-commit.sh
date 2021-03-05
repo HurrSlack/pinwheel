6d40c0960c85367494786fa50d3cacb90cf0078a
James Zetlen <jzetlen@adobe.com>
2021-03-05 11:35:10 -0600

    initial v2 commit

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -5,15 +5,13 @@ BlackBg="\e[30m"
 Green="\e[32m"
 Yellow="\e[33m"
 Red="\e[91m"
-if [[ -x ./node_modules/.bin/eslint ]]
-then
-	if ./node_modules/.bin/eslint .
-	then
-		printf "\n${BlackBg}${Green}Lint passed, honeybunch. Committing...${Reset}\n"
-	else
-		 printf "${BlackBg}${Red}Lint failed. Aborted commit.${Reset}\n\n"
-		exit 1
-	fi
+if [[ -x ./node_modules/.bin/eslint ]]; then
+  if ./node_modules/.bin/eslint lib/; then
+    printf "\n${BlackBg}${Green}Lint passed, honeybunch. Committing...${Reset}\n"
+  else
+    printf "${BlackBg}${Red}Lint failed. Aborted commit.${Reset}\n\n"
+    exit 1
+  fi
 else
-	printf "\n${BlackBg}${Yellow}CANNOT LINT BECAUSE LOCAL ESLINT IS NOT INSTALLED!!! Committing anyway, but run \`npm install\`, pal.${Reset}\n\n";
+  printf "\n${BlackBg}${Yellow}CANNOT LINT BECAUSE LOCAL ESLINT IS NOT INSTALLED!!! Committing anyway, but run \`npm install\`, pal.${Reset}\n\n"
 fi
