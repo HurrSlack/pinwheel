e713fae78e97488c5f874e70b2b1997c2656c071
James Zetlen <jzetlen@adobe.com>
2021-03-22 09:23:24 -0500

    chore: remove old app and reorganize project structure

--- scripts/install-hooks.js
+++ scripts/install-hooks.js
@@ -1,30 +1,31 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 const knownHooks = [
-  'applypatch-msg',
-  'pre-applypatch',
-  'post-applypatch',
-  'pre-commit',
-  'prepare-commit-msg',
-  'commit-msg',
-  'post-commit',
-  'pre-rebase',
-  'post-checkout',
-  'post-merge',
-  'pre-receive',
-  'update',
-  'post-receive',
-  'post-update',
-  'pre-auto-gc',
-  'post-rewrite',
-  'pre-push'
+  "applypatch-msg",
+  "pre-applypatch",
+  "post-applypatch",
+  "pre-commit",
+  "prepare-commit-msg",
+  "commit-msg",
+  "post-commit",
+  "pre-rebase",
+  "post-checkout",
+  "post-merge",
+  "pre-receive",
+  "update",
+  "post-receive",
+  "post-update",
+  "pre-auto-gc",
+  "post-rewrite",
+  "pre-push",
 ];
-const basename = (n) => path.basename(n, '.sh');
+const basename = (n) => path.basename(n, ".sh");
 const isHook = (n) => knownHooks.indexOf(basename(n)) !== -1;
-const newHooks = (n) => path.join(__dirname, `hooks/${n || ''}`);
-const hooks = (n) => path.join(__dirname, `.git/hooks/${n || ''}`);
-const isGHook = (hook) => isHook(hook) &&
-  fs.readFileSync(hook, 'utf-8').indexOf('#!/usr/bin/env node') === 0;
+const newHooks = (n) => path.join(__dirname, `hooks/${n || ""}`);
+const hooks = (n) => path.join(__dirname, `.git/hooks/${n || ""}`);
+const isGHook = (hook) =>
+  isHook(hook) &&
+  fs.readFileSync(hook, "utf-8").indexOf("#!/usr/bin/env node") === 0;
 
 const toDisable = fs.readdirSync(hooks()).map(hooks).filter(isGHook);
 const disableHook = (hook) => {
@@ -34,7 +35,7 @@ const disableHook = (hook) => {
 if (toDisable.length > 0) {
   process.stdout.write(`${toDisable.length} old ghooks to disable...`);
   toDisable.forEach(disableHook);
-  process.stdout.write('Done!\n');
+  process.stdout.write("Done!\n");
 }
 
 const toAdd = fs.readdirSync(newHooks()).filter(isHook);
@@ -43,13 +44,12 @@ const addHook = (name) => {
   const hookName = basename(name);
   const hook = hooks(hookName);
   fs.writeFileSync(hook, fs.readFileSync(newHook));
-  fs.chmodSync(hook, '744');
+  fs.chmodSync(hook, "744");
   process.stdout.write(` ${hookName} `);
 };
 
 if (toAdd.length > 0) {
   process.stdout.write(`${toAdd.length} hooks to install...`);
   toAdd.forEach(addHook);
-  process.stdout.write('Done!\nHooks installed. Write good codes, errbody!\n');
+  process.stdout.write("Done!\nHooks installed. Write good codes, errbody!\n");
 }
-
